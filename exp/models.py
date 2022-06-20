c4ac4a55f708104c23645edc46bd24178b3fbd47
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2022-06-20 19:52:55 +0300

    Add raw version of EAV for ModelOrganism

--- exp/models.py
+++ exp/models.py
@@ -1,12 +1,82 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+########################
+#     Managers         #
+########################
+
+class OrganismMapManager(models.Manager):
+    def update_for_object(self, obj, **kwargs):
+        '''
+        Sets custom field types and their values for obj.
+        kwargs provides Descriptor:DescriptorValue pairs.
+        If the provided Descriptor or DescriptorValue does not exist
+        it will be created.
+        '''
+
+        current_organism = self.get_for_object(
+            obj).values_list('organism_name__name', flat=True)
+
+        for organism, organism_val in kwargs.items():
+            org = organism.lower()
+            if org in current_organism and organism_val == '':
+                OrganismValue._default_manager.remove_for_object(obj, org)
+            else:
+                # FIXME: This is bad, because it doesn't check if field_val is the same...
+                # But it is enforced at the database level through unique constraints.
+                OrganismValue._default_manager.add_for_object(
+                    obj, org, organism_val)
+
+    def get_by_model(self, queryset_or_model, filters={}):
+        try:
+            queryset = queryset_or_model._default_manager.objects.all()
+        except AttributeError:
+            queryset = queryset_or_model
+        # FIXME: ``organism`` is hardcoded here - needs to be changed!
+        organism = filters.get('organism', '')
+        
+        organism_value_qs = OrganismValue.objects.filter(
+            value__icontains=filters.get('value', ''), organism=organism)
+        
+        organism_value = self.filter(organism_value__in=organism_value_qs, organism_name=organism,
+                                 object_id__in=queryset.values_list('id', flat=True))
+        return [ct.object for ct in organism_value]
 
 
-# Managers
+class OrganismValueManager(models.Manager):
+    
+    def remove_for_object(self, obj, organism_name, val=None):
+        if val is not None:
+            OrganismMap._default_manager.get_for_object(obj).filter(
+                organism_name__name=organism_name, organism_value__value=val).delete()
+        else:
+            OrganismMap._default_manager.get_for_object(obj).filter(
+                organism_name__name=organism_name).delete()
+
+    def add_for_object(self, obj, organism_name, val):
+        '''
+        Creates new custom OrganismValue for a given obj and Organism.
+        '''
+        organism, created = Organism._default_manager.get_or_create(
+            name=organism_name)
+        value, created = self.get_or_create(value=val, organism=organism)
+        ctype = ContentType.objects.get_for_model(obj)
+        
+        OrganismMap._default_manager.get_or_create(
+            organism_name=organism_name,
+            organism_value=value,
+            content_type=ctype,
+            object_id=obj.pk
+        )
 
 
 
-# Create your models here.
+#####################
+#       Models      #
+#####################
 
 class Project(models.Model):
     title = models.CharField(max_length=150)
@@ -34,19 +104,69 @@ class ExpPlatform(models.Model):
     def __str__(self):
         return f'{self.title}-{self.n_reads}-{self.length_libtype}'
 
+##############################
+#   EAV for ModelOrganism    #
+##############################
 
-class Organism:
-    pass
+class Organism(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    class Meta:
+        verbose_name = 'organism'
+    
+    def __str__(self):
+        return f'{self.name}'
 
-class OrganismDescriptor(models.Model):
-    pass 
+
+class OrganismValue(models.Model):
+    
+    organism = models.ForeignKey(Organism, on_delete=models.CASCADE)
+    value = models.CharField(db_index=True, max_length=255)
+    
+    objects = OrganismValueManager()
+
+    class Meta:
+        unique_together = ('organism', 'value')
+    
+    
+class OrganismMap(models.Model):
+    organism = models.ForeignKey(
+        Organism,
+        db_index=True,
+        on_delete=models.CASCADE
+    )
+    
+    organism_value = models.ForeignKey(
+        OrganismValue,
+        db_index=True,
+        on_delete=models.CASCADE
+    )
+    
+    content_type = models.ForeignKey(
+        ContentType,
+        verbose_name='model organism',
+        db_index=True,
+        on_delete=models.CASCADE
+    )
+    
+    object_id = models.PositiveIntegerField(db_index=True)
+    object = GenericForeignKey('content_type', 'object_id')
+
+    objects = OrganismMapManager()
 
 
 class ModelOrganism(models.Model):
-
+    name = models.CharField(max_length=200)
+    custom_fields = GenericRelation(OrganismMap)
     
     def __str__(self):
-        return f'{self.id}'
+        return f'{self.name}'
+
+
+#####################
+#   Final Model     #
+#####################
+
 
 class Experiment(models.Model):
     data_filepath = models.CharField(max_length=250)
