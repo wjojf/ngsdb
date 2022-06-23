7d960765fd5609f9af9c0fe15107b8f446cfdf31
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2022-06-23 12:46:55 +0300

    Register Descrtiptor Models

--- exp/admin.py
+++ exp/admin.py
@@ -5,6 +5,12 @@ from exp import models as exp_models
 admin.site.register(exp_models.Experiment)
 admin.site.register(exp_models.Project)
 admin.site.register(exp_models.PrepMethod)
+
+admin.site.register(exp_models.Descriptor)
+admin.site.register(exp_models.DescriptorValue)
+admin.site.register(exp_models.DescriptorMap)
 admin.site.register(exp_models.ModelOrganism)
+
+
 admin.site.register(exp_models.ExpPlatform)
 
