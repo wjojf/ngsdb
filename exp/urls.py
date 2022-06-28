f4bccee31dc4eb00d8dc5ff07e1eeefb058a9f93
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2022-06-28 17:30:34 +0300

    rewrite views using classes

--- exp/urls.py
+++ exp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from exp import views 
 
 urlpatterns = [
-    path('', views.home, name='exp-home'),
-    path('all', views.allExperiments, name='exp-all'),
-    path('<int:pk>', views.singeExperiment, name='exp-single'),
-    path('update-organism', views.updateOrganism, name='create-organism'),
+    path('', views.HomeView.as_view(), name='exp-home'),
+    path('all', views.ExperimentsView.as_view(), name='exp-all'),
+    path('<int:exp_id>', views.ExperimentView.as_view(), name='exp-single'),
+    path('update-organism', views.DescriptorMapFormView.as_view(), name='create-descriptor'),
     
 ]
